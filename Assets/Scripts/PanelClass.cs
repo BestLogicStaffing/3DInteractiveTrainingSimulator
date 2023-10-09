5353a44cf690252dfd2f2778eacbc09ba8a5316e
Brendan Trieu <bktrieu@gmail.com>
2023-10-09 12:35:55 -0700

    Brendan: testing from different email

--- Assets/Scripts/PanelClass.cs
+++ Assets/Scripts/PanelClass.cs
@@ -14,5 +14,5 @@ public class PanelClass : MonoBehaviour
     public int panel_type; //0 for one with image, 1 with no image
     public GameObject panel_object, sprite; //panel_object is the actual panel containing the sprite, name_text, and message_text.
                                             //sprite is the picture that is displayed
-    public TMP_Text name_text, message_text;
+    public TMP_Text name_text, message_text; //where the name and messages will appear
 }
